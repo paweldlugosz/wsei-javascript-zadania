6a255d31d2d11c04c6712342a056b3df225507b9
paweldlugosz <48649957+paweldlugosz@users.noreply.github.com>
2019-11-10 13:50:09 +0100

    Add files via upload

---
+++ 1_Zadania/obowiązkowe/5_this/js/app.js
@@ -0,0 +1,40 @@
+// Zadanie 0
+
+const car = {
+    brand: "aaaa",
+    color: "red",
+    numberOfKilometers: 0,
+
+    printCarInfo: function() {
+        return this.brand + " " + this.color + " " + this.numberOfKilometers;
+    },
+
+    drive: function(kilometers) {
+        this.numberOfKilometers += kilometers;
+    }
+}
+
+
+console.log(car.printCarInfo());
+car.drive(20);
+console.log(car.printCarInfo());
+
+
+// Zadanie 1
+
+car.list = [
+    '2018:10:10',
+    '2017:10:10',
+    '2016:10:10'
+];
+
+car.appednDate = function(date) {
+    this.list.push(date);
+}
+
+car.getList = function() {
+    return this.list;
+}
+
+car.appednDate('2019:10:10');
+console.log(car.getList());
