0e39234d2ba67d1b351cc60b08decddf426cb9c5
paweldlugosz <48649957+paweldlugosz@users.noreply.github.com>
2019-11-24 14:16:17 +0100

    Add files via upload

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    /*
-    Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
-     */
+    // Zad 0
+    let elemClass = document.getElementsByClassName('title')[0];
 
+    function getDataAnimation(element) {
+        let newVariable = element.getAttribute('data-animation');
+        return newVariable;
+    }
+
+    console.log(getDataAnimation(elemClass));
+
+    // Zad 1
+    let el1 = document.getElementById('home');
+    let el2 = document.querySelector('#home');
+    let el3 = document.querySelector('li[data-direction]');
+    let elClassBlock = document.querySelector('.block');
+
+    // Zad 2
+
+    let el4 = document.querySelector('nav li');
+    let el5 = document.querySelector('div p');
+    let el6 = document.querySelector('article div');
 });
