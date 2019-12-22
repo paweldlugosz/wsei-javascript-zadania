fde7a710d2e7461f21aba424a0ff70e9bc8a536f
paweldlugosz <48649957+paweldlugosz@users.noreply.github.com>
2019-12-22 14:01:41 +0100

    Add files via upload

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,43 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+
+    // Zadanie 0
+
+    const elements = document.querySelectorAll('.ex5 li');
+
+    for (let i = 0; i < elements.length; i++) {
+        if (i % 2 == 0) {
+            elements[i].style.backgroundColor = 'green';
+        }
+
+        if (i % 3 == 0) {
+         elements[i].style.textDecoration = 'underline';
+        }
+    }
+
+    elements[4].className = 'big';
+
+    // Zadanie 1
+
+    let chrome = document.querySelector('.chrome');
+    let edge = document.querySelector('.edge');
+    let firefox = document.querySelector('.firefox');
+
+    console.log(chrome);
+    console.log(edge);
+    console.log(firefox);
+
+    edge.style.backgroundImage = 'url(./assets/img/edge.png)';
+    firefox.style.backgroundImage = 'url(./assets/img/firefox.png)';
+
+    
+    // dokończyć.............
+
+
+
+    // Zadanie 4
+    
+
+
 });
